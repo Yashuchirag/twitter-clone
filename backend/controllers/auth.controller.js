7aeee34522336289902b6d439c4940d7e9ba0783
Yashuchirag <chiragchandrashekar@gmail.com>
2025-09-15 12:32:07 -0700

    First commit with project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = (req, res) => {
+    res.json({
+        data: "You are in signup"
+    })
+};
+
+export const login = (req, res) => {
+    res.json({
+        data: "You are in login"
+    })
+};
+
+export const logout = (req, res) => {
+    res.json({
+        data: "You are in logout"
+    })
+};
---
+++ backend/db/connectMongoDB.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectMongoDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL)
+        console.log("MongoDB connected", conn.connection.host)
+    } catch (error) {
+        console.log("MongoDB connection error", error);
+        process.exit(1);
+    }
+}
+
+export default connectMongoDB;
